6d36d56fc3311433f0b24a63f6d4b7d27d6c8fd7
anneliese040 <mrs.anneliese.schroeder@gmail.com>
2026-04-06 22:42:41 +0200

    Datenbank-Initialisieren-3

--- Datenbank/db_module.py
+++ Datenbank/db_module.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Aktuellen Ordner zum Python-Suchpfad hinzufügen
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from db_module import init_db
 
 if __name__ == "__main__":
