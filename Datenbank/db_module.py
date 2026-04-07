79f79b795e2ca65103e221b3c96b9fc93576998d
anneliese040 <mrs.anneliese.schroeder@gmail.com>
2026-04-07 21:56:59 +0200

    Skript-1

--- Datenbank/db_module.py
+++ Datenbank/db_module.py
@@ -1,11 +1,100 @@
-import sys
-import os
+Kontext-Manager
 
-# Aktuellen Ordner zum Python-Suchpfad hinzufügen
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from contextlib import contextmanager
+import sqlite3
 
-from db_module import init_db
+DB_NAME = "pi_datenbank.db"
 
-if __name__ == "__main__":
-    init_db()
-    print("Datenbank initialisiert!")
+@contextmanager
+def get_db_connection():
+    
+    conn = sqlite3.connect(DB_NAME)
+    try:
+        yield conn
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
+    finally:
+        conn.close()
+
+
+Tabellen erstellen
+
+def init_db():
+    with get_db_connection() as conn:
+        cursor = con.cursor()
+        cursor.excute("""
+            CREATE TABLE IF NOT EXISTS stats (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp TEXT NOT NULL,
+                cpu REAL NOT NULL,
+                ram REAL NOT NULL,
+            )
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS warnings (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                timestamp TEXT NOT NULL,
+                component TEXT NOT NULL,
+                value REAL NOT NULL
+            )
+        """)
+
+
+Stats speichern
+
+def save_stat(timestamp, cpu, ram):
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO stats (timestamp, cpu, ram) VALUES (?,?,?)",
+            (timestamp, cpu, ram)
+        )
+
+
+Warnungen speichern
+
+def save_warning(timestamp, component, value):
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO warnings (timestamp, component, value) VALUES (?,?,?)",
+            (timestamp, component, value)
+        )
+
+
+Letzte Stats abrufen
+
+def get_recent_stats(limit=10):
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id, timestamp, cpu, ram FROM stats ORDER BY id DESC LIMIT?",
+            (limit,)
+        )
+        return cursor.fetchall()
+    
+
+Letzte Warnungen abrufen
+
+def get_recent_stats(limit=10):
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id, timestamp, component, value FROM warnings ORDER BY id DESC LIMIT?",
+            (limit,)
+        )
+        return cursor.fetchall()
+    
+
+Alle Warnungen abrufen
+
+def get_all_warnings():
+    with get_db_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT id, timestamp, component, value FROM warnings ORDER BY timestamp ASC"
+        )
+        return cursor.fetchall()
